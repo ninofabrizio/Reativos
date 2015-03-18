6fbfc8059ef57153b89f5d33e960d8ea0ccd541a
Nino Fabrizio Tiriticco Lizardo <nin0_w3@hotmail.com>
2015-03-18 20:46:39 -0300

    Parte da tarefa 3 da aula 2
    Tarefa 3 - Aula 2

---
+++ tarefa3_aula2.ino
@@ -0,0 +1,16 @@
+// Primeira versao
+
+#include "event_driven.c"
+
+#define LED_PIN 13
+#define BUT_PIN 2
+
+void button_changed (int pin, int v) {
+    
+    digitalWrite(LED_PIN, v);
+}
+
+void init () {
+
+    button_listen(BUT_PIN);
+}
