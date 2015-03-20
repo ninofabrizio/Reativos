8878be5442805e6a3a6df4a57c5a94cd20007d89
Nino Fabrizio Tiriticco Lizardo <nin0_w3@hotmail.com>
2015-03-20 18:14:57 -0300

    tarefa 3 - aula 2 - versao 3
    versao 3

--- event_driven.c
+++ event_driven.c
@@ -1,32 +1,36 @@
-// Segunda versao
+// Terceira versao
+// Obs.: Daonde vem o ms? Da tarefa 2??
+//#define ms 10000
 
-#define ms 10000
-
-//Timer
+// Timers
 // Teste: supostamente posso usar estas variaveis como timers que possa resetar quando queira
 extern volatile unsigned long timer0_overflow_count;
+extern volatile unsigned long timer1_overflow_count;
 
 /*
 unsigned long t1;
+unsigned long t2;
 */
 
-// Pin
-int pin;
+// Pin do botao
+int bPin;
 
 // Pin passado gera notificacoes
 void button_listen (int pin) {
   
-    //...
+    bPin = pin;
+    pintMode(bPin, INPUT);
 }
   
 // Timer expira após ms milisegundos
+// Nao terminado, falta associar com o que tem que ser feito na tarefa 2...
 void timer_set (int ms) {
      
     timer0_overflow_count = 0;
 }
     
-void button_changed (int pin, int v);
-void timer_expired (void);
+void button_changed (int pin, int v);  // Notifica pin mudado para modo 'v'
+void timer_expired (void);  // Notifica timer expirado
     
 // Programa principal  
     
@@ -39,14 +43,16 @@ void setup () {
       
 void loop () {
   
+    int v = digitalRead(bPin);
+    
     // Detectando botao pressionado
-    if(digitalRead(BUT_PIN))
-      button_changed();
+    if(v)
+      button_changed(bPin, v);
     
     // Vendo se timer deve expirar
     if(timer0_overflow_count >= ms) {
     
-      timer_set();
+      timer_set(ms);
       timer_expired();
     }
 }
