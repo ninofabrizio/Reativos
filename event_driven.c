b7d7efe006fa5a5728900d21ab11fc5a8f748f2e
Nino Fabrizio Tiriticco Lizardo <nin0_w3@hotmail.com>
2015-03-26 13:57:37 -0300

    Mais atualizado com a parte de timers (inacabado)

--- event_driven.c
+++ event_driven.c
@@ -1,42 +1,88 @@
-// Terceira versao
-// Obs.: Daonde vem o ms? Da tarefa 2??
-//#define ms 10000
+// Quarta versao
+
+// Obs.: As partes que lidam com os timers ainda nao estao acabadas. O malloc de lista nao foi testado, mas aparentemente
+// o Arduino aceita malloc
+
+// Duvidas:
+// - Os papeis dos timers (cada um eh ativado conforme seu respectivo botao eh apertado?)
+// - Onde controlar as piscadas do led? ja que aparentemente os timers ficam na API event_driven e o tarefa2 nao passa o pin
+//   do led (neste caso, posso fazer uma funcao que passe o pin do led pra ele?)
+
+#include <stdlib.h>
+
+// Lista para pin de buttons
+typedef struct listPin {
+  
+    int bPin;
+    struct listPin *prox;
+    struct listPin *ant;
+} listP;
+
+typedef struct cabecaLista {
+	
+	listP *ini;	
+	listP *corrent;
+} cbcLista;
+
+cbcLista cabeca;
 
 // Timers
-// Teste: supostamente posso usar estas variaveis como timers que possa resetar quando queira
-extern volatile unsigned long timer0_overflow_count;
-extern volatile unsigned long timer1_overflow_count;
+extern volatile unsigned long timer1;
+extern volatile unsigned long timer2;
 
 /*
 unsigned long t1;
 unsigned long t2;
 */
 
-// Pin do botao
-int bPin;
+// Pin
+//int bPin;
+
+// Intervalor de tempo
+unsigned long intMs;
 
 // Pin passado gera notificacoes
 void button_listen (int pin) {
   
-    bPin = pin;
-    pintMode(bPin, INPUT);
+    listP no = (listP *)malloc(sizeof(listP));
+    
+    no->bPin = pin;
+    no->prox = NULL;
+    no->ant = NULL;
+  
+    if(cabeca->ini == NULL) {
+      
+      cabeca->ini = no;  
+      cabeca->corrent = no;
+    }
+    else {
+      
+      cabeca->corrent->prox = no;
+      no->ant = cabeca->corrent;
+    }
+
+    pinMode(no->bPin, INPUT);
 }
   
-// Timer expira após ms milisegundos
-// Nao terminado, falta associar com o que tem que ser feito na tarefa 2...
+// Timer expira após ms milisegundos passados por usuario
 void timer_set (int ms) {
      
-    timer0_overflow_count = 0;
+    intMs = ms;
 }
     
 void button_changed (int pin, int v);  // Notifica pin mudado para modo 'v'
-void timer_expired (void);  // Notifica timer expirado
+void timer1_expired (void);  // Notifica timer 1 expirado
+void timer2_expired (void); // Notifica timer 2 expirado
     
-// Programa principal  
+// Inicio do Programa principal  
     
 void setup () {
       
-    //timer0_overflow_count = millis();
+    //timer1 = millis();
+    
+    cabeca = (cbcLista *)malloc(sizeof(cbcLista));
+    
+    cabeca->ini = NULL;
     
     init();
 }
@@ -50,9 +96,8 @@ void loop () {
       button_changed(bPin, v);
     
     // Vendo se timer deve expirar
-    if(timer0_overflow_count >= ms) {
+    if(timer1 >= intMs) {
     
-      timer_set(ms);
-      timer_expired();
+      timer1_expired();
     }
 }
