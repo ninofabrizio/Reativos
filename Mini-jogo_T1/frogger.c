00bfc9f4cfcc456b1b52e2cf2582b02d6e85ac55
ninofabrizio <nin0_w3@hotmail.com>
2015-05-12 17:00:28 -0300

    Jogo versao em C

---
+++ Mini-jogo_T1/frogger.c
@@ -0,0 +1,240 @@
+#include <SDL2/SDL.h>
+#include <assert.h>
+
+#define NUMMINIONS 3
+
+// Desenha tudo
+void drawAll (SDL_Renderer *renderer, SDL_Rect frog, SDL_Rect *enem, int mov) {
+
+	SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderFillRect(renderer, NULL);
+
+	SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0x00);
+	SDL_RenderDrawLine(renderer, 0, 408, 640, 408);
+	SDL_RenderDrawLine(renderer, 0, 68, 640, 68);
+
+	for(int i = 0; i < NUMMINIONS; i++)
+		SDL_RenderFillRect(renderer, &enem[i]);
+
+	if(mov == 0)
+		SDL_SetRenderDrawColor(renderer, 0x00,0xFF,0x00,0x00);
+	else if(mov == -1)
+		SDL_SetRenderDrawColor(renderer, 0xFF,0x00,0x00,0x00);
+	else
+		SDL_SetRenderDrawColor(renderer, 0x00,0x00,0xFF,0x00);
+
+	SDL_RenderFillRect(renderer, &frog);
+
+    SDL_RenderPresent(renderer);
+
+}
+
+// Vê se sapo tocou/foi tocado por inimigo
+int verifyDeath (SDL_Rect frog, SDL_Rect *enem) {
+
+	for(int i = 0; i < NUMMINIONS; i++)
+		if(frog.y > enem[i].y && (frog.y + frog.h < enem[i].y + enem[i].h))
+			if((frog.x <= enem[i].x && enem[i].x <= frog.x + frog.w) || 
+			   (frog.x >= enem[i].x && frog.x + frog.w <= enem[i].x + enem[i].w) || 
+			   (frog.x <= enem[i].x + enem[i].w && enem[i].x + enem[i].w <= frog.x + frog.w))
+				return -1;
+	return 0;
+}
+
+/*
+// Coloca sapo e inimigos em suas posições iniciais de movimento
+void initialPos (SDL_Rect frog, SDL_Rect *enem, unsigned int *enemThen) {
+
+	int i;
+
+	frog.x = 333;
+	frog.y = 429;
+
+	for(i = 0; i < NUMMINIONS; i++) {
+		enem[i].x = 0;
+		enem[i].y = 344 - (68 * 2 * i);
+	}
+
+	enemThen[0] = SDL_GetTicks();
+	for(i = 1; i < NUMMINIONS; i++)
+		enemThen[i] = enemThen[i-1];
+}
+*/
+
+int main (int argc, char *args[]) {
+
+    /* INITIALIZATION */
+
+	// Tempo total de execução da library
+	unsigned int now;
+
+	// Última vez que inimigos se mexeram
+	unsigned int enemThen[NUMMINIONS];
+
+	// Movimento do sapo
+	unsigned int frogMov = 0;
+
+	// Parâmetro de loop
+	bool running = true;
+
+	// Controladores de botões
+	bool uPressed = false, dPressed = false, lPressed = false, rPressed = false, reset = false;
+
+	// Eventos
+	SDL_Event e;
+	SDL_KeyboardEvent *ke;
+
+    int i, err = SDL_Init(SDL_INIT_EVERYTHING);
+    assert(err == 0);
+
+    SDL_Window *window = SDL_CreateWindow("F R O G G E R", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 476, SDL_WINDOW_SHOWN);
+    assert(window != NULL);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    assert(renderer != NULL);
+
+    /* EXECUTION */
+
+	SDL_Rect frog = {333, 429, 26, 26};
+
+	SDL_Rect enem[NUMMINIONS];
+	for(i = 0; i < NUMMINIONS; i++) {
+		enem[i].h = 60;
+		enem[i].w = 100;
+		enem[i].x = 0;
+		enem[i].y = 344 - (68 * 2 * i);
+	}
+
+//	initialPos(frog, enem, enemThen);
+
+	drawAll(renderer, frog, enem, frogMov);
+
+	enemThen[0] = SDL_GetTicks();
+	for(i = 1; i < NUMMINIONS; i++)
+		enemThen[i] = enemThen[i-1];
+
+	// Executa ações até a janela ser fechada
+    while(running) {
+
+		SDL_WaitEventTimeout(&e, 50);
+
+		// Tratador de eventos
+		switch(e.type) {
+
+			// Tratando botão apertado
+			case SDL_KEYDOWN:
+				ke = (SDL_KeyboardEvent *) &e;
+
+				switch(ke->keysym.sym){
+                    case SDLK_LEFT:
+						if(frog.x - 32 > 0 && frogMov == 0 && lPressed == false && rPressed != true && uPressed != true && dPressed != true) {
+							lPressed = true;
+							frog.x -= 32;
+							frogMov = verifyDeath(frog, enem);
+						}
+                        break;
+                    case SDLK_RIGHT:
+						if(frog.x + 32 + frog.w < 640 && frogMov == 0 && rPressed == false && lPressed != true && uPressed != true && dPressed != true) {
+							rPressed = true;
+							frog.x += 32;
+							frogMov = verifyDeath(frog, enem);
+						}
+                        break;
+                    case SDLK_UP:
+						if(frog.y - 68 > 0 && frogMov == 0 && uPressed == false && rPressed != true && lPressed != true && dPressed != true) {
+							uPressed = true;
+							frog.y -= 68;
+							frogMov = verifyDeath(frog, enem);
+							if(frog.y <= 68)
+								frogMov = 1;
+						}
+                        break;
+                    case SDLK_DOWN:
+						if(frog.y + 68 + frog.h < 476 && frogMov == 0 && dPressed == false && rPressed != true && uPressed != true && lPressed != true) {
+							dPressed = true;
+							frog.y += 68;
+							frogMov = verifyDeath(frog, enem);
+						}
+                        break;
+					case SDLK_r:
+						if(reset == false) {
+							reset = true;
+							frogMov = 0;
+							//initialPos(frog, enem, enemThen);
+							frog.x = 333;
+							frog.y = 429;
+
+							for(i = 0; i < NUMMINIONS; i++) {
+								enem[i].x = 0;
+								enem[i].y = 344 - (68 * 2 * i);
+							}
+
+							enemThen[0] = SDL_GetTicks();
+							for(i = 1; i < NUMMINIONS; i++)
+								enemThen[i] = enemThen[i-1];
+						}
+						break;
+                    default:
+                        break;
+				}
+				break;
+
+			case SDL_KEYUP:
+				ke = (SDL_KeyboardEvent *) &e;
+
+				switch(ke->keysym.sym){
+                    case SDLK_LEFT:
+						lPressed = false;
+                        break;
+                    case SDLK_RIGHT:
+						rPressed = false;
+                        break;
+                    case SDLK_UP:
+						uPressed = false;
+                        break;
+                    case SDLK_DOWN:
+						dPressed = false;
+                        break;
+					case SDLK_r:
+						reset = false;
+						break;
+                    default:
+                        break;
+				}
+				break;
+
+			// Tratando saída de janela
+			case SDL_QUIT:
+				running = false;
+				break;
+
+			default:
+				break;
+		}
+
+		now = SDL_GetTicks();
+
+		for(i = 0; i < NUMMINIONS; i++) {
+
+			if(now - enemThen[i] > (100 - (25 * i)) && frogMov == 0) {
+
+				enem[i].x += 1 + (6 * i);
+				if(enem[i].x + enem[i].w > 640)
+					enem[i].x = 0;
+
+				frogMov = verifyDeath(frog, enem);
+
+				enemThen[i] = now;
+			}
+		}
+
+		drawAll(renderer, frog, enem, frogMov);
+	}
+
+    /* FINALIZATION */
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+	return 0;
+}
